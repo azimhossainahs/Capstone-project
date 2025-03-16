d6b17e42455b98b6abdee9561ce207ba502b2ee6
azimhossain <azimhossain.ahs@gmail.com>
2025-03-16 17:09:51 +0600

    generated a node for studenr

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
-#include "ikl.h"
-int main(){
-    int a = 20;
-    int b = 30;
-    printf("%d\n", a+b);
-    printf("%d", b-a);
-    sbsdcbcv
-    sss
-    ssssss
+//udf data type node
+typedef struct node
+{
+    int id;
+    struct node *next;
+} node;
+int main()
+{
+    node *head = malloc(sizeof(node));
+    head->next = NULL;
+    if(head == NULL ){
+        printf("Memory allocation not possible\n");
+        return 1;
+    }
+    while(1){
+        int action;
+        printf("1. Enroll(add) student\n2. Remove Student\n3. exit\n");
+        printf("Enter your Action: ...");
+        scanf("%d", &action);
+        if(action == 1){
+            int st_id;
+            printf("#######Enroll new Student########");
+            printf("Enter student id: ");
+            scanf("%d", &st_id);
+            addStudent(head, st_id);
+        }
+    }
+    
 }
